ed80e04c2ed6c81cae556605fda731107fda8d69
jkpark <jkpark3070@gmail.com>
2015-03-16 17:02:57 +0900

    before cgv

--- net.cho.web/src/main/resources/smarthomepage/com/db/member_data.sql
+++ net.cho.web/src/main/resources/smarthomepage/com/db/member_data.sql
@@ -1,14 +1,14 @@
-﻿INSERT INTO MEMBER
-VALUES ('test01@naver.com','1234','홍길동','010-1234-5678');
 INSERT INTO MEMBER
-VALUES ('test02@naver.com','1234','이율곡','010-1234-5678');
+VALUES ('test01@naver.com','1234','홍길동,'010-1234-5678');
 INSERT INTO MEMBER
-VALUES ('test03@naver.com','1234','이순신','010-1234-5678');
+VALUES ('test02@naver.com','1234','임꺽정,'010-1234-5678');
 INSERT INTO MEMBER
-VALUES ('test04@naver.com','1234','이황','010-1234-5678');
+VALUES ('test03@naver.com','1234','유관순','010-1234-5678');
 INSERT INTO MEMBER
-VALUES ('test05@naver.com','1234','신사임당','010-1234-5678');
+VALUES ('test04@naver.com','1234','김구','010-1234-5678');
 INSERT INTO MEMBER
-VALUES ('test06@naver.com','1234','김구','010-1234-5678');
+VALUES ('test05@naver.com','1234','윤봉길','010-1234-5678');
 INSERT INTO MEMBER
-VALUES ('test07@naver.com','1234','윤봉길','010-1234-5678');
+VALUES ('test06@naver.com','1234','이순신','010-1234-5678');
+INSERT INTO MEMBER
+VALUES ('test07@naver.com','1234','이이','010-1234-5678');
